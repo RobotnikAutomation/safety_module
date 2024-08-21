3f4059ca9823ea1960be306830ed137dc93d83ec
Rafael Martin <rmartin@robotnik.es>
2024-08-21 19:09:24 +0200

    registers/v1/bool: added triggered bool

--- safety_module/registers/v1/bool.py
+++ safety_module/registers/v1/bool.py
@@ -1,11 +1,14 @@
 from . import RegisterBase
 
+from time import sleep
+
 class Bool(RegisterBase):
     def __init__(self, name, description, config):
         super().__init__(name, description)
         self._config = config
         self.__address = config['address']
         self.__kind = config['kind']
+        self.__trigger = config['trigger'] if 'trigger' in config else False
         self.set_context(False)
 
 
@@ -17,7 +20,10 @@ class Bool(RegisterBase):
     def write(self, value, set_value_callback):
         if self.__kind == 'input':
             print(f'Cannot write to input register {self.get_name()}')
-        else:
-            print(f'Writing {self.get_name()} to {self.__address} with value {value}')
-            set_value_callback(self.__address, value)
-            self.set_context(value)
+            return
+
+        if self.__trigger and value:
+            set_value_callback(self.__address, False)
+            sleep(0.25)
+        set_value_callback(self.__address, value)
+        self.set_context(value)
